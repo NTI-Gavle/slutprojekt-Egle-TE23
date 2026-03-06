c66964c823436310ea3853c78f25a03b108196d8
SPUCEEE <egle20161130@gmail.com>
2026-03-06 10:53:51 +0100

    öppnade projekt, la till css färg variabler

--- includes/header.php
+++ includes/header.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($pageTitle) ? htmlspecialchars($pageTitle) : 'My Home Project' ?></title>
-
-    <!-- CSS -->
     <link rel="stylesheet" href="css/styles.css">
-
-    <!-- JS -->
     <script src="js/app.js" defer></script>
 </head>
 <body>
