55e4a0579e075a49eafcbf10586ef74af0d921c8
SPUCEEE <egle20161130@gmail.com>
2026-03-30 13:43:23 +0200

    fixade header funktion. Header visas när man scrollar upp och gömms när man scrollar ner

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,10 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("header").style.top = "0px";
-  } else {
-    document.getElementById("header").style.top = "-100px";
-  }
+if (this.oldScroll > this.scrollY) {
+    document.querySelector("header").style.transform = "translateY(0px)"; 
+} else {
+    document.querySelector("header").style.transform = "translateY(-100px)";
+}
+this.oldScroll = this.scrollY; 
 }
